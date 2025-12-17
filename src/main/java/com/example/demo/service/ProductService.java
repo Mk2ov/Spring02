ae86792c1fde4729422d4795b154a4c225cee158
Oleg Makarikhin <bigbossmr4@gmail.com>
2025-12-17 14:18:32 +0200

    laba2

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product update(Long id, Product product) {
+        product.setId(id);
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
